38b380cb2c97a06e7eddfd07f79a9656a35a1c24
Harry Sebastian Putra Prijanto <harry_sebastian@hotmail.com>
2024-09-30 00:01:53 +0700

    Seeder Update
    Questions udah lengkap buat 2 film tambahan (Deadpool sama Medium)

--- database/seeders/AnswerSeeder.php
+++ database/seeders/AnswerSeeder.php
@@ -109,6 +109,192 @@ class AnswerSeeder extends Seeder
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
 
+        ],[//Deadpool and Wolverine Question 1
+            'question_id' => '4',
+            'answer_text' => 'Chris Evans',
+            'is_correct' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '4',
+            'answer_text' => 'Johny Depp',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '4',
+            'answer_text' => 'Elijah Wood',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '4',
+            'answer_text' => 'Robert Downey Jr.',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],[//Deadpool and Wolverine Question 2
+            'question_id' => '5',
+            'answer_text' => 'Red Skull',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '5',
+            'answer_text' => 'Thanos',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '5',
+            'answer_text' => 'Magneto',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '5',
+            'answer_text' => 'Cassandra Nova',
+            'is_correct' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],[//Deadpool and Wolverine Question 3
+            'question_id' => '6',
+            'answer_text' => 'Tom Holland',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '6',
+            'answer_text' => 'Tom Bradley',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '6',
+            'answer_text' => 'Henry Cavill',
+            'is_correct' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '6',
+            'answer_text' => 'Hugh Jackman',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],[//The Medium 1
+            'question_id' => '7',
+            'answer_text' => 'Thailand',
+            'is_correct' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '7',
+            'answer_text' => 'Japan',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '7',
+            'answer_text' => 'Indonesia',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '7',
+            'answer_text' => 'South Korea',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],[//The Medium 2
+            'question_id' => '8',
+            'answer_text' => 'Time travel',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '8',
+            'answer_text' => 'A family haunted by spirits',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '8',
+            'answer_text' => 'A cursed artifact',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '8',
+            'answer_text' => 'A shaman dealing with a possession',
+            'is_correct' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],[//The Medium 3
+            'question_id' => '9',
+            'answer_text' => 'Bong Joon-ho',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '9',
+            'answer_text' => 'Na Hong-jin',
+            'is_correct' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '9',
+            'answer_text' => 'Kim Jee-woon',
+            'is_correct' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
+        ],
+        [
+            'question_id' => '9',
+            'answer_text' => 'Park Chan-wook',
+            'is_correct' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+
         ]]
         );
     }
--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -13,7 +13,7 @@ class QuestionSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('questions')->insert([[
+        DB::table('questions')->insert([[ //AOS
             'quiz_id' => 1,
             'question_text' => 'What is the title of Part 2 of the K-drama Alchemy of Souls?',
             'created_at' => Carbon::now(),
@@ -30,7 +30,41 @@ class QuestionSeeder extends Seeder
             'question_text' => 'Which actor plays the male lead character, Jang Uk, in Alchemy of Souls?',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
+        ],[ //Deadpool n Wolverine
+            'quiz_id' => 2,
+            'question_text' => 'Which actor portrays Johnny Storm in the Deadpool & Wolverine movies?',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
         ],
+        [
+            'quiz_id' => 2,
+            'question_text' => 'Who is the main antagonist of the movie ?',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ],
+        [
+            'quiz_id' => 2,
+            'question_text' => 'Which actor plays the Wolverine alternative "Calverine" ?',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ],[ //The Medium
+            'quiz_id' => 3,
+            'question_text' => 'The Medium is a horror movie from which country?',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ],
+        [
+            'quiz_id' => 3,
+            'question_text' => 'What is the main theme of the movie The Medium?',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ],
+        [
+            'quiz_id' => 3,
+            'question_text' => 'The Medium was co-produced by which famous South Korean filmmaker?',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]
         ]);
     }
 }
